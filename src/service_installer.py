4fc521b3e5d1adfc2c6667a8e77314ab9b955471
yaelitrovnik <yaelitrovnik@gmail.com>
2025-08-20 20:34:37 +0300

    fixing bash and errors

--- src/service_installer.py
+++ src/service_installer.py
@@ -10,7 +10,7 @@ logging.basicConfig(
 
 def install_service(vm_name):
     try:
-        subprocess.run(['bash', 'scripts/install_nginx.sh', vm_name], check=True)
+        subprocess.run(['wsl', 'bash', 'scripts/install_nginx.sh', vm_name], check=True)
         logging.info("Service installation completed successfully on {vm_name} .")
     except subprocess.CalledProcessError as e:
         logging.error(f"Error occurred during service installation on {vm_name}: {e}")
