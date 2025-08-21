8be69299b28972e5b8dd8a1246626882b416438e
yaelitrovnik <yaelitrovnik@gmail.com>
2025-08-21 11:22:44 +0300

    editing install_service

--- src/service_installer.py
+++ src/service_installer.py
@@ -1,5 +1,6 @@
 import subprocess
 import logging
+import platform
 
 # Make sure logging is configured
 logging.basicConfig(
@@ -10,7 +11,11 @@ logging.basicConfig(
 
 def install_service(vm_name):
     try:
-        subprocess.run(['wsl', 'bash', 'scripts/install_nginx.sh', vm_name], check=True)
+        #subprocess.run(['wsl', 'bash', 'scripts/install_nginx.sh', vm_name], check=True)
+        if platform.system() == "Windows":
+            cmd = ["wsl", "bash", "scripts/install_nginx.sh", vm_name]    
+        else:
+            cmd = ["bash", "scripts/install_nginx.sh", vm_name]
         logging.info("Service installation completed successfully on {vm_name} .")
     except subprocess.CalledProcessError as e:
         logging.error(f"Error occurred during service installation on {vm_name}: {e}")
